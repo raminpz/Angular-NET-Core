4787f44cb32d78e1c6990f242189082f02907a1a
raminpz <raminp.1012@gmail.com>
2023-02-26 16:12:11 -0500

    Interpolacion, Property Binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'FrontEnd';
+  nombre = 'Ramiro';
+  textoPlaceholder = 'Escriba algo aqui';
+  deshabilitado = true;
+  imgSrc = 'https://upload.wikimedia.org/wikipedia/commons/thumb/c/cf/Angular_full_color_logo.svg/640px-Angular_full_color_logo.svg.png';
+
+  constructor(){
+    setInterval(() => this.deshabilitado = false, 3000);
+  }
+
+  getSuma(numero1: number, numero2: number){
+    return numero1 + numero2;
+  }
 }
+
+
