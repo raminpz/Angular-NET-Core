d3556c62f3eac71acea824a920dedc237ad7a5a0
raminpz <raminp.1012@gmail.com>
2023-02-26 16:38:23 -0500

    data binding

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  texto = 'Esto es un video sobre event bindings'
-
-  cambiarTexto(): void{
-    this.texto = 'En el proximo video vamos a ver Two way data-binding';
-  }
+  texto = '';
 }
 
 
